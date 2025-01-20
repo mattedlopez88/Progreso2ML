60e72ccccac48dfce91c54b3fd0a7592dfeafaaa
Matias Eduardo Lopez Peña <matias.lopez@udla.edu.ec>
2025-01-20 11:03:54 -0500

    commit 3

--- Progreso2ML/MainPage.xaml.cs
+++ Progreso2ML/MainPage.xaml.cs
@@ -1,39 +1,24 @@
 ﻿using Newtonsoft.Json;
 using Progreso2ML.Models;
+using Progreso2ML.ViewModel;
 
 namespace Progreso2ML
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new ViewModelML();
+
         }
 
         private async void OnCounterClicked(object sender, EventArgs e)
         {
-            ModelML dto = null;
-            HttpResponseMessage response;
-
-            string requestURL = $"https://api.weatherxu.com/v1/weather?api_key=b3a0d538a908b97cd9e620172fe482c7&lat=40.7128&lon=-74.0060";
-            try
+            var viewModel = BindingContext as ViewModelML;
+            if (viewModel != null)
             {
-                using (HttpClient client = new HttpClient())
-                {
-                    response = await client.GetAsync(requestURL);
-                    if (response.IsSuccessStatusCode)
-                    {
-                        string json = await response.Content.ReadAsStringAsync();
-                        dto = JsonConvert.DeserializeObject<ModelML>(json);
-                    }
-                }
-                
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                await viewModel.GetInfo();
             }
         }
     }
---
+++ Progreso2ML/ViewModel/ViewModelML.cs
@@ -0,0 +1,114 @@
+﻿using Progreso2ML.Models;
+using Progreso2ML.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Progreso2ML.ViewModel;
+
+public class ViewModelML : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public ViewModelML()
+    {
+        dateTime = DateTime.Now;
+        title = string.Empty;
+        latitude = string.Empty;
+        longitude = string.Empty;
+        service = new ServiceML();
+    }
+
+    private DateTime dateTime;
+    public DateTime ChosenDate
+    {
+        get { return dateTime; }
+        set
+        {
+            if (value != dateTime)
+            {
+                dateTime = value;
+                NotifyPropertyChanged();
+            }
+            _ = GetInfo();
+        }
+    }
+
+    private string title;
+    public string Title
+    {
+        get { return title; }
+        set
+        {
+            if (value != title)
+            {
+                title = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    private string latitude;
+    public string Latitude
+    {
+        get { return latitude; }
+        set
+        {
+            if (latitude != value)
+            {
+                latitude = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    private string longitude;
+    public string Longitude
+    {
+        get { return longitude; }
+        set
+        {
+            if (longitude != value)
+            {
+                longitude = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+
+    private ServiceML service;
+    public ServiceML Service
+    {
+        get
+        {
+            if (service == null)
+            {
+                service = new ServiceML();
+            }
+            return service;
+        }
+    }
+
+    private async Task GetInfo()
+    {
+        ModelML dto = await Service.GetWeather();
+        if (dto == null)
+        {
+            Title = "error";
+        }
+        else
+        {
+            Latitude = dto.latitude;
+            Longitude = dto.longitude;
+        }
+    }
+}
