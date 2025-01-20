c5c307b980de92305d8a6b21cc4c8713b80c6945
Matias Eduardo Lopez Peña <matias.lopez@udla.edu.ec>
2025-01-20 10:21:37 -0500

    committ

---
+++ Progreso2ML/Models/ModelML.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progreso2ML.Models
+{
+    public class ModelML
+    {
+        public string lat { get; set; }
+        public string lon { get; set; }
+    }
+}
---
+++ Progreso2ML/Services/ServiceML.cs
@@ -0,0 +1,41 @@
+﻿using Progreso2ML.Models;
+using Newtonsoft.Json;
+using Stripe.Forwarding;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using ThreadNetwork;
+
+namespace Progreso2ML.Services
+{
+    public class ServiceML
+    {
+        public async Task<ModelML> GetWeather()
+        {
+            ModelML dto = null;
+            HttpResponseMessage response;
+
+            string requestURL = $"https://api.weatherxu.com/v1/weather?api_key=b3a0d538a908b97cd9e620172fe482c7&lat=40.7128&lon=-74.0060";
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    response = await client.GetAsync(requestURL);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dto = JsonConvert.DeserializeObject<ModelML>(json);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return dto;
+        }
+    }
+}
